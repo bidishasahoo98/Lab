86fc6b3ca4e645b50170c59ed0665a009a4c4002
bidishasahoo98 <110465869+bidishasahoo98@users.noreply.github.com>
2022-12-08 15:19:31 +0530

    Update ExtractArrayList.java

--- 17th Nov/ExtractArrayList.java
+++ 17th Nov/ExtractArrayList.java
@@ -3,7 +3,7 @@
 package Arraylist;
 import java.util.*;
 
-public class ExtractArrayList {
+public class ExtractArrayList {           //create class
 	  public static void main(String[] args) {
 	  // Create a list and add some colors to the list
 	  ArrayList<String> a1 = new ArrayList<String>();
@@ -12,9 +12,9 @@ public class ExtractArrayList {
 	  a1.add("Orange");
 	  a1.add("White");
 	  a1.add("Black");
-	  System.out.println("Original list: " + a1);
-	  List<String> sub_List = a1.subList(0, 3);
-	  System.out.println("List of first three elements: " + sub_List);
+	  System.out.println("Original list: " + a1);      //print original Arraylist
+	  List<String> sub_List = a1.subList(0, 3);         //extract a portion of an array
+	  System.out.println("List of first three elements: " + sub_List);  //print extract array
 	 }
 	}
 
